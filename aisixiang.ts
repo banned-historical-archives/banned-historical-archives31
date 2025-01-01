13490860041af22a0d686540c27dd6d89cb43f06
Your Name <you@example.com>
2025-01-02 01:10:09 +0800

    test

---
+++ aisixiang.ts
@@ -0,0 +1,16 @@
+export default {
+    "entity": {
+      "id": "aisixiang",
+      "name": "爱思想",
+      "internal": false,
+      "official": false,
+      "type": "db",
+      "author": "",
+      "files": []
+    },
+    "parser_option": {},
+    "parser_id": "aisixiang",
+    "path": "html",
+    "resource_type": "book",
+    "version": 2
+  };
